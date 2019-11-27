0d82a56be5700834fc632a558009a06f258be5d2
Jay Wen <jaywen89@gmail.com>
2019-11-27 21:00:59 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,4 +1,5 @@
 require 'directors_database'
+require 'pry'
 
 # Write a method that, given an NDS creates a new Hash
 # The return value should be like:
@@ -6,11 +7,22 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  result = {}
-  nil
+  result = {[:name],[:movies]}
+  
 end
-
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  total = 0
+  index = 0
+  director_data.each do |director|   #single director
+    while index < director_data[:movies].length do 
+      total += director_data[:movies][index][:worldwide_gross]
+      index += 1
+    end
+    # director_data.each do |director|
+    # director[:movies]
+  end
+total
 end
+
